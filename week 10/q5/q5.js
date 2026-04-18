7069278893131e94e016b401aa27bbc87bdabb1a
Yugant Adhikari <ayugant25@tbc.edu.np>
2026-04-18 19:11:56 +0545

    final commit

--- week 10/q5/q5.js
+++
@@ -1,5 +0,0 @@
-// A for loop to run 4 times
-for (let i = 1; i <= 4; i++) {
-    // Generate an image tag for each number (1.jpg, 2.jpg, etc.)
-    document.write(`<img src="${i}.jpg" alt="${i}.jpg" style="width:200px; margin:10px;">`);
-}
